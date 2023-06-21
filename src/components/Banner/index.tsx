fccc7bc11fce019a36145f53699564b05d5f3ec7
Paola Freitas <pahnetone@Paolas-MacBook-Air.local>
2023-06-21 17:59:29 -0300

    feat Cart component

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -7,15 +7,10 @@ import Button from '../Button'
 import { Game } from '../../pages/Home'
 
 import { formataPreco } from '../ProductsList'
+import { useGetFeaturedGameQuery } from '../../services/api'
 
 const Banner = () => {
-  const [game, setGame] = useState<Game>()
-
-  useEffect(() => {
-    fetch('https://fake-api-tau.vercel.app/api/eplay/destaque')
-      .then((res) => res.json())
-      .then((res) => setGame(res))
-  }, [])
+  const { data: game, isLoading } = useGetFeaturedGameQuery()
 
   if (!game) {
     return <h3>Carregando...</h3>
